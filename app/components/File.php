2c406b866da4d90179f65803d7a3c8315f225a24
Archimandrit <35890919+Archimandrit@users.noreply.github.com>
2018-03-15 16:31:07 +0300

    Add files via upload

--- app/components/File.php
+++ app/components/File.php
@@ -1,44 +1,56 @@
 <?php
 
+namespace app\components;
 
 class File
 {
-    const  ROOT_PATH=_DIR_.'/../../';
+    const ROOT_PATH = __DIR__.'/../../';
 
-
-
-    public  static function cache($data, $uniqDate, $prefix)
-    {
+    public static function cache($data, $uniqDate, $prefix) {
         $filename = md5($uniqDate);
-        $dir = 'var/cache'.$prefix.'/';
+        $dir = "/var/cache/$prefix/";
         self::createFile($filename, $dir, $data);
     }
 
-    public  static function getCache($uniqDate, $prefix)
-    {
+    public static function getCache($uniqDate, $prefix) {
         $filename = md5($uniqDate);
-        $dir = 'var/cache'.$prefix.'/';
+        $dir = "/var/cache/$prefix/";
         $filePath = self::ROOT_PATH.$dir.$filename;
-        if (file_exists($filePath)){
+        if (!file_exists($filePath)) {
             return null;
         }
         return file_get_contents($filePath);
     }
 
-
-    public static function createFile($filename, $dir, $data)
+    public static function log($data,$uniqData, $prefix)
     {
+        $filename = md5($uniqData);
+        $dir = "/var/cache/$prefix";
+        self::createFile($filename, $dir, $data);
+    }
+
+    public static function createFile($filename, $dir, $data) {
         self::createDir($dir);
-        $result = file_put_contents(self::ROOT_PATH .$dir.$filename, $data);
+        $result = file_put_contents(self::ROOT_PATH.$dir.$filename, $data);
         chmod(self::ROOT_PATH.$dir.$filename, 0777);
         return $result;
     }
 
-    public static function createDir($dir)
-    {   $dir = self::ROOT_PATH . $dir;
-        if(!file_exists($dir)){
+    public static function createLog($filename, $dir, $data)
+    {
+        self::createDir($dir);
+        $result = fopen($filename, "a+");
+        fwrite($data);
+        fclose($filename);
+        return $result;
+    }
+
+    public static function createDir($dir) {
+        $dir = self::ROOT_PATH . $dir;
+        if (!file_exists($dir)) {
             mkdir($dir, 0777, true);
             chmod($dir, 0777);
         }
     }
+
 }
